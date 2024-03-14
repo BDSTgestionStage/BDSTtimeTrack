4080fc1a65006224b65c6fcab762cadb71a9ad91
SodoyerKyllian <kyllian.sodoeyr@campus-la-chataigneraie.org>
2024-03-14 13:37:09 +0100

    Ajoutez des fichiers projet.

---
+++ DataService.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+
+namespace TimeTrack
+{
+    public class DataServiceSQL
+    {
+        private readonly SqlConnection _connection;
+
+        public DataServiceSQL()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.UserID = "sa";
+            builder.Password = "Info76240#";
+            builder.InitialCatalog = "MLR1";
+            builder.DataSource = "localhost";
+            _connection = new SqlConnection(builder.ConnectionString);
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TimeTrack
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
