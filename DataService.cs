dcf4f8d7990b51b85c21e94048aa449a97a24912
Tom.B <88104750+TomB76@users.noreply.github.com>
2024-03-20 10:21:29 +0100

    tkt

--- DataService.cs
+++ DataService.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.SqlClient;
+﻿using System.Data;
+using System.Data.SqlClient;
 
 namespace TimeTrack
 {
@@ -11,7 +12,7 @@ namespace TimeTrack
             SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
             builder.UserID = "sa";
             builder.Password = "Info76240#";
-            builder.InitialCatalog = "MLR1";
+            builder.InitialCatalog = "BDST_TimeTrack";
             builder.DataSource = "localhost";
             _connection = new SqlConnection(builder.ConnectionString);
         }
@@ -20,14 +21,32 @@ namespace TimeTrack
         {
             try
             {
+                SqlCommand command = new SqlCommand("TimeTrack_Connexion", _connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@username", "username_value");
+                command.Parameters.AddWithValue("@password", "password_value");
+
                 _connection.Open();
-                // Vous devriez utiliser des requêtes paramétrées pour éviter les injections SQL
-                string query = "SELECT COUNT(1) FROM Utilisateur WHERE UTI_Prenom=@username AND UTI_MotDePasse=@password";
-                SqlCommand command = new SqlCommand(query, _connection);
-                command.Parameters.AddWithValue("@username", username);
-                command.Parameters.AddWithValue("@password", password); 
-                int result = (int)command.ExecuteScalar();
-                return result > 0;
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    bool isValidUser = Convert.ToBoolean(reader["IsValidUser"]);
+                    int userId = Convert.ToInt32(reader["UTI_ID"]);
+
+                    if (isValidUser)
+                    {
+                        Console.WriteLine("Login successful! User ID: " + userId);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid username or password.");
+                    }
+                }
+
+                reader.Close();
             }
             catch (Exception ex)
             {
@@ -41,3 +60,4 @@ namespace TimeTrack
         }
     }
 }
+
