b73ff61015a28c916166eed38fa8e12ba51cbf92
LphqhOryhu <hugo.de-brabander@campus-la-chataigneraie.org>
2024-03-26 16:45:59 +0100

    added login admin user + c

---
+++ AdminPage.xaml.cs
@@ -0,0 +1,86 @@
+using System;
+using Microsoft.Maui.Controls;
+using System.Security.Cryptography;
+using System.Text;
+using System.Collections.Generic;
+
+namespace TimeTrack
+{
+    public partial class AdminPage : ContentPage
+    {
+        private string roleId;
+        public AdminPage()
+        {
+            InitializeComponent();
+        }
+
+        private static string GenerateSHA256Hash(string input)
+        {
+            // Utilisation de SHA256.Create pour obtenir une instance de SHA256
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // Convertir le mot de passe en bytes
+                byte[] bytes = Encoding.UTF8.GetBytes(input);
+
+                // Hasher les bytes du mot de passe
+                byte[] hashBytes = sha256Hash.ComputeHash(bytes);
+
+                // Convertir le hash en string hexadécimal
+                string hash = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
+
+                return hash;
+            }
+        }
+
+        private async void OnRegisterClicked(object sender, EventArgs e)
+        {
+            int id;
+
+            string nom = NomEntry.Text;
+            string prenom = PrenomEntry.Text;
+            string auth = AuthEntry.Text;
+            roleId = RoleEntry.Text;
+            string motDePasse = PasswordEntryCreate.Text;
+            string motDePasseHash = GenerateSHA256Hash(motDePasse);
+            var rolesValides = new List<string> { "Administrateur", "Utilisateur" };
+
+            if (!int.TryParse(Id.Text, out id))
+            {
+                // Afficher un message d'erreur si la conversion échoue
+                await DisplayAlert("Erreur", "L'ID doit être un nombre entier.", "OK");
+                return; // Sortir de la méthode si la conversion a échoué
+            }
+
+            if (!rolesValides.Contains(roleId))
+            {
+                await DisplayAlert("Erreur", "Veuillez saisir un rôle valide (Administrateur ou Utilisateur).", "OK");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(motDePasse))
+            {
+                await DisplayAlert("Erreur", "Le mot de passe ne peut pas être vide.", "OK");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(motDePasseHash))
+            {
+                await DisplayAlert("Erreur", "Le hachage du mot de passe a échoué.", "OK");
+                return;
+            }
+
+            DataServiceSQL dataService = new DataServiceSQL();
+            bool userAdded = dataService.AddUser(id, nom, prenom, motDePasseHash, auth, roleId);
+
+            if (userAdded)
+            {
+                await DisplayAlert("Succès", "Utilisateur enregistré avec succès.", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Erreur", "L'enregistrement a échoué. diya", "OK");
+            }
+        }
+    }
+
+}
--- DataService.cs
+++ DataService.cs
@@ -1,4 +1,8 @@
-﻿using System.Data.SqlClient;
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace TimeTrack
 {
@@ -16,22 +20,78 @@ namespace TimeTrack
             _connection = new SqlConnection(builder.ConnectionString);
         }
 
-        public bool AuthenticateUser(string username, string password)
+        public Tuple<bool, string> AuthenticateUser(string username, string password)
         {
             try
             {
                 _connection.Open();
-                // Vous devriez utiliser des requêtes paramétrées pour éviter les injections SQL
-                string query = "SELECT COUNT(1) FROM Utilisateur WHERE UTI_Prenom=@username AND UTI_MotDePasse=@password";
+                // Note : Il est préférable de ne sélectionner que les champs nécessaires plutôt que d'utiliser SELECT *
+                string query = "SELECT UTI_Role, UTI_Prenom, UTI_MotDePasse FROM Utilisateur WHERE UTI_Prenom=@username AND UTI_MotDePasse=@password";
                 SqlCommand command = new SqlCommand(query, _connection);
                 command.Parameters.AddWithValue("@username", username);
-                command.Parameters.AddWithValue("@password", password); 
-                int result = (int)command.ExecuteScalar();
+                command.Parameters.AddWithValue("@password", password);
+
+                // ExecuteScalar retourne la première colonne de la première ligne dans le jeu de résultats retourné par la requête
+                // Dans ce cas, il s'agit de UTI_Role
+                var roleIdObject = command.ExecuteScalar();
+
+                _connection.Close();
+
+                // Si roleIdObject n'est pas null, cela signifie que l'utilisateur a été authentifié avec succès
+                if (roleIdObject != null)
+                {
+                    string roleId = roleIdObject.ToString();
+                    // Authentification réussie, retourner true et le roleId
+                    return Tuple.Create(true, roleId);
+                }
+                else
+                {
+                    // Authentification échouée, retourner false et null pour le roleId
+                    return Tuple.Create(false, (string)null);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Gérer l'exception ici (par exemple, en loguant l'erreur)
+                // Ne pas oublier de fermer la connexion dans un bloc finally pour s'assurer qu'elle sera fermée quelle que soit l'issue de la tentative de connexion
+                return Tuple.Create(false, (string)null);
+            }
+            finally
+            {
+                if (_connection.State == ConnectionState.Open)
+                {
+                    _connection.Close();
+                }
+            }
+        }
+
+
+
+        public bool AddUser(int id, string nom, string prenom, string motDePasse, string auth, string roleId)
+        {
+            try
+            {
+                _connection.Open();
+               
+                // Préparation de la commande SQL avec des paramètres
+                string query = "INSERT INTO Utilisateur (UTI_ID, UTI_Nom, UTI_Prenom, UTI_MotDePasse, UTI_Auth, UTI_Role) VALUES (@ID, @Nom, @Prenom, @MotDePasse, @Auth, @RoleId)";
+                SqlCommand command = new SqlCommand(query, _connection);
+
+                // Ajout des valeurs aux paramètres
+                command.Parameters.AddWithValue("@ID", id);
+                command.Parameters.AddWithValue("@Nom", nom);
+                command.Parameters.AddWithValue("@Prenom", prenom);
+                command.Parameters.AddWithValue("@MotDePasse", motDePasse);
+                command.Parameters.AddWithValue("@Auth", auth);
+                command.Parameters.AddWithValue("@RoleId", roleId);
+
+                // Exécution de la commande
+                int result = command.ExecuteNonQuery();
                 return result > 0;
             }
             catch (Exception ex)
             {
-                // Gérer l'exception (log l'erreur, par exemple)
+                // Gérer l'exception ici (log l'erreur, etc.)
                 return false;
             }
             finally
@@ -39,5 +99,10 @@ namespace TimeTrack
                 _connection.Close();
             }
         }
+
+        
+
     }
+
 }
+
